5ace1e6db77bc47d0aefa4832de7c0be93157fae
freiremilena <fcamilenafr@gmail.com>
2021-11-23 00:04:39 -0300

    adding footer

--- main.js
+++ main.js
@@ -14,15 +14,27 @@ for (const link of links) {
     nav.classList.remove("show");
   });
 }
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
-
-window.addEventListener("scroll", function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight;
   if (window.scrollY >= navHeight) {
     header.classList.add("scroll");
   } else {
     header.classList.remove("scroll");
   }
+}
+function backToTop() {
+  const backToTopButton = document.querySelector(".back-to-top");
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop();
 });
 
 const swiper = new Swiper(".swiper", {
@@ -33,3 +45,15 @@ const swiper = new Swiper(".swiper", {
   mousewheel: true,
   keyboard: true,
 });
+
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  "#home .text, #home .image #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonials #contact .text, #contact .links, #footer .brand, footer .social",
+  { interval: 100 }
+);
